2a3e9aa1d1952c0b0518eeeb3ee3d21079b48f67
Wayne Sun <wtse.sun@gmail.com>
2019-05-29 19:33:29 -0700

    implement proxy server

---
+++ server/app.js
@@ -0,0 +1,29 @@
+// Dependencies
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const bodyParser = require('body-parser');
+
+// Config
+const { routes } = require('./config.json');
+
+const app = express();
+
+const PORT = 8000;
+
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../public/dist`));
+
+for (route of routes) {
+    app.use(route.route,
+        proxy({
+            target: route.address,
+            pathRewrite: (path, req) => {
+                return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
+            }
+        })
+    );
+}
+
+app.listen(PORT, () => {
+    console.log('Proxy listening on port ' + PORT);
+});
